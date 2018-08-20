7f431d2c2a85de184ea35a5c0055bdc263854299
Elisa Demanez <demanez.elisa@gmail.com>
2018-08-20 11:42:55 +0200

    + global lang var

--- src/main.js
+++ src/main.js
@@ -11,10 +11,26 @@ Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
+
+const shared = {
+  lang: "fr"
+}
+
+shared.install = function(){
+  Object.defineProperty(Vue.prototype, '$lang', {
+    get () { return shared }
+  })
+}
+
+Vue.use(shared);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  data: {
+  
+  },
   components: { App },
   template: '<App/>'
 })
