1773eb7b9a6d973c33b3bdeb2eb284f30c831512
Elisa Demanez <demanez.elisa@gmail.com>
2018-09-05 15:47:00 +0200

    + admin : can create marker w/ inputs and map

--- src/views/Admin/Point.vue
+++ src/views/Admin/Point.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="">
+    <div id="bbonjour">
         <Navbar />
         <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
-                {{point}}
                 <h2 class="headline oswald font-weight-bold" v-if="point">
                     Modification du point
                 </h2>
@@ -12,16 +11,33 @@
                 </h2>
 
 
-                 <v-form v-model="valid">
-                    <v-text-field
-                    v-model="point.name_fr"
-                    :counter="50"
-                    label="Titre"
-                    required
-                    ></v-text-field>
+                <v-form v-model="valid">
+                    <v-text-field v-model="point.name_fr" :counter="50" label="Titre" required></v-text-field>
                     <!-- :rules="titreRules" -->
-      
+                    <v-text-field v-model="point.description_fr" label="Description" required></v-text-field>
+
+                    <div id="map">
+                    </div>
+                    <v-layout row>
+                        <v-flex xs12 sm10 >
+                            <v-text-field v-model="point.latitude" label="Latitude"  :rules="rules.latitude"  required ></v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm10 offset-sm2>
+
+
+                            <v-text-field v-model="point.longitude" label="Longitude"  :rules="rules.longitude"   required></v-text-field>
+
+                        </v-flex>
+
+                    </v-layout>
+                    <v-text-field v-model="point.name_en" :counter="50" label="Titre anglais" ></v-text-field>
+                    <!-- :rules="titreRules" -->
+                    <v-text-field v-model="point.description_en" label="Description anglaise" ></v-text-field><v-text-field v-model="point.name_nl" :counter="50" label="Titre néerlandais" ></v-text-field>
+                    <!-- :rules="titreRules" -->
+                    <v-text-field v-model="point.description_nl" label="Description néerlandaise" ></v-text-field>
+
                 </v-form>
+
             </v-flex>
         </v-layout>
     </div>
@@ -30,6 +46,8 @@
 <script>
 import Navbar from "@/components/Admin/Navbar";
 import MainService from "@/services/MainService";
+import "leaflet";
+const L = window.L;
 
 export default {
   name: "Point",
@@ -39,26 +57,79 @@ export default {
   data() {
     return {
       point: {
+        map: [],
         id: null,
+        latitude: 44.4986865,
+        longitude: 1.1861205,
         name_fr: null,
         name_en: null,
-        name_nl: null
+        name_nl: null,
+        description_fr: null,
+        description_en: null,
+        description_nl: null
       },
-      valid: false
+      valid: false,
+      rules: {
+        latitude: value => {
+          const pattern = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/;
+          return pattern.test(value) || "Invalid latitude.";
+        },
+        longitude: value => {
+          const pattern = /^\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)/;
+          return pattern.test(value) || "Invalid longitude.";
+        }
+      },
+      marker: null
     };
   },
   methods: {
     async getPoint() {
       const response = await MainService.fetchPoint(this.$route.params.id);
       this.point = response.data;
-      //   console.log("getpoint")
+    },
+
+    initMap() {
+      var self = this;
+      var map = L.map("map").setView([44.4986865, 1.1861205], 14);
+
+      L.tileLayer("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  
+      }).addTo(map);
+      this.map = map;
+    },
+    initMarker(lat, long) {
+      var self = this;
+      this.marker = L.marker([lat, long], {
+        draggable: true
+      });
+      this.marker.addTo(self.map);
+      this.marker.on("move", function() {
+        self.point.latitude = self.marker["_latlng"].lat;
+        self.point.longitude = self.marker["_latlng"].lng;
+      });
     }
   },
-  created: function() {
+  watch: {
+    point: {
+      handler: function(after, before) {
+        var self = this;
+        var newLatLng = new L.LatLng(
+          parseFloat(self.point.latitude),
+          parseFloat(self.point.longitude)
+        );
+        this.marker.setLatLng(newLatLng);
+      },
+      deep: true
+    }
+  },
+
+  mounted: function() {
     const self = this;
+    // if update
     if (this.$route.params.id) {
       this.getPoint().then(function() {
-        // console.log("1erthen")
         for (const key in self.point.name) {
           if (self.point.name.hasOwnProperty(key)) {
             const element = self.point.name[key];
@@ -91,14 +162,22 @@ export default {
             }
           }
         }
-        console.log(self.point);
+        self.initMap();
+        self.initMarker(self.point.latitude, self.point.longitude);
       });
+    } else {
+      self.initMap();
+      self.initMarker(self.point.latitude, self.point.longitude);
     }
   }
 };
 </script>
 
 <style scoped>
+#map {
+  width: 650px;
+  height: 400px;
+}
 </style>
 
 
